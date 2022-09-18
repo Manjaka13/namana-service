ed181ab9eb45f1ac5a5b1fb96b2954e3b4a8b844
Hari <manjaka.rajaonson@gmail.com>
2022-09-18 15:13:52 +0300

    add constants

---
+++ const.js
@@ -0,0 +1,7 @@
+/**
+ * Export constant variables from here
+ */
+
+export const CRYPTO_ITERATIONS = process.env.CRYPTO_ITERATIONS || 310000;
+export const CRYPTO_KEYLEN = process.env.CRYPTO_KEYLEN || 32;
+export const CRYPTO_DIGEST = process.env.CRYPTO_DIGEST || "sha256";
