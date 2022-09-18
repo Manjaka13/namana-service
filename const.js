38ef97b27e3d3ec63b94b894ceac2e9f90122077
Hari <manjaka.rajaonson@gmail.com>
2022-09-18 15:27:11 +0300

    add constants

--- const.js
+++ const.js
@@ -5,3 +5,9 @@
 export const CRYPTO_ITERATIONS = process.env.CRYPTO_ITERATIONS || 310000;
 export const CRYPTO_KEYLEN = process.env.CRYPTO_KEYLEN || 32;
 export const CRYPTO_DIGEST = process.env.CRYPTO_DIGEST || "sha256";
+export const DATABASE_FOLDER = "./db";
+export const SESSION_SECRET = process.env.SESSION_SECRET || "selena tehdi";
+export const SESSION_DATABASE = "session.db";
+export const MAIN_DATABASE = "namanagasy.db";
+export const MASTER_USER_NAME = process.env.MASTER_USER_NAME || "root";
+export const MASTER_USER_PASSWORD = process.env.MASTER_USER_PASSWORD || "root";
